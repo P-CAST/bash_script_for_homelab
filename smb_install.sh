07364023ea15d60ce5984129d23aa5d664f7c0e1
P-CAST <vggdggez@gmail.com>
2021-12-06 12:36:56 +0700

    CNG/ADD: grammar/wipe disk case

--- smb_install.sh
+++ smb_install.sh
@@ -20,32 +20,39 @@ sudo apt-get -qq install samba samba-common-bin -y > /dev/null 2>&1
 echo "PASS: install Samba"
 
 # create Samba directory
-sudo mkdir /nas > /dev/null 2>&1
-sudo chmod 777 /nas
-echo "PASS: create directory"
+echo "\nEnter folder name"
+read FolderName
+sudo mkdir /$FolderName > /dev/null 2>&1
+sudo chmod 777 /$FolderName
+echo "PASS: create directory, name: $FolderName"
 
 # mount drive
 lsblk -nd --output name,size | grep sd*
-echo "\nselect disk to use *choose wrong drive can cause system failure*"
+echo "\nSelect disk to use *choose wrong drive can cause system failure*"
 read DiskName
-sudo mkfs.ext4 /dev/$DiskName > /dev/null 2>&1
-sudo mount -t auto /dev/$DiskName /nas
-echo "PASS:mount $DiskName to /nas successful"
+echo "\nDo you want to wipe all data on the disk?[y/N]"
+read DelAns
+case "$DelAns" in
+  [yY]) sudo mkfs.ext4 /dev/$DiskName > /dev/null 2>&1 && echo "PASS: $DiskName has been wiped";;
+  [nN]) echo "PASS: No disk wiped";;
+  * ) echo "PASS: No disk wiped";;
+esac
+sudo mount -t auto /dev/$DiskName /$FolderName
+echo "PASS:mount $DiskName to /$FolderName successful"
 
 # edit Samba config
-echo "\nenter server name"
+echo "\nEnter server name"
 read ServerName
-
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bck
 sudo rm /etc/samba/smb.conf
 sudo cp /etc/samba/smb.conf.bck /etc/samba/smb.conf
-echo "config file backup at /etc/samba/smb.conf.bck"
-sudo echo -en "[$ServerName]\npath=/nas\nbrowseable=yes\nread only=no\nwriteable=yes\ncreate mask=0777\ndirectory mask=0777\npublic=no\nforce user=root" >> /etc/samba/smb.conf
-sudo rm -rf /nas/lost+found > /dev/null 2>&1
+echo "Samba config file got backup at /etc/samba/smb.conf.bck"
+sudo echo -en "[$ServerName]\npath=/$FolderName\nbrowseable=yes\nread only=no\nwriteable=yes\ncreate mask=0777\ndirectory mask=0777\npublic=no\nforce user=root" >> /etc/samba/smb.conf
+sudo rm -rf /$FolderName/lost+found > /dev/null 2>&1
 echo "PASS: edit Samba config"
 
 # add user to Samba
-echo "\nenter username you want to connect with"
+echo "\nEnter the username you want to connect with"
 read UserName
 sudo adduser --system --no-create-home --disabled-password --gecos "" $UserName > /dev/null 2>&1
 sudo passwd -d $UserName > /dev/null 2>&1
@@ -58,5 +65,5 @@ sudo /etc/init.d/smbd restart
 sudo /etc/init.d/nmbd restart
 
 # auto mount drive on reboot
-sudo echo "/dev/&DiskName /nas auto defaults,user 0 2"
+sudo echo -en "/dev/$DiskName /$FolderName auto defaults,user 0 2" >> /etc/fstab
 echo "PASS: auto mount drive on reboot\n"
